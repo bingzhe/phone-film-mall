dfb0d1d0244a2bc97cb9df6c47eb8cbfc15a84d4
‘bingzhe’ <874177815@qq.com>
2023-03-07 18:08:25 +0800

    地址管理

--- pages/select-address/index.js
+++ pages/select-address/index.js
@@ -1,19 +1,37 @@
-const WXAPI = require("apifm-wxapi");
+// const WXAPI = require("apifm-wxapi");
 const AUTH = require("../../utils/auth");
-import { getAddress } from "../../api/api";
+import { getAddress, saveAddress } from "../../api/api";
 
 const app = getApp();
 Page({
   data: {},
-  
+
   selectTap: function (e) {
     console.log(e);
-    var id = e.currentTarget.dataset.id;
-    WXAPI.updateAddress({
+    const id = e.currentTarget.dataset.id;
+    const selectAddress = this.data.addressList.find(
+      (item) => item.address_id == id
+    );
+
+    const postData = {
       token: wx.getStorageSync("token"),
-      id: id,
-      isDefault: "true",
-    }).then(function (res) {
+      name: selectAddress.name,
+      address: selectAddress.address,
+      telephone: selectAddress.telephone,
+      province_name: selectAddress.province_name,
+      province_id: selectAddress.province_id,
+      city_name: selectAddress.city_name,
+      city_id: selectAddress.city_id,
+      area_name: selectAddress.area_name,
+      area_id: selectAddress.area_id,
+      address_id: id,
+      is_default: 1,
+    };
+
+    /**
+     * 修改默认地址
+     */
+    saveAddress(postData).then((res) => {
       wx.navigateBack({});
     });
   },
@@ -26,7 +44,6 @@ Page({
 
   editAddess: function (e) {
     console.log(e);
-
     wx.navigateTo({
       url: "/pages/address-add/index?id=" + e.currentTarget.dataset.id,
     });
@@ -46,8 +63,13 @@ Page({
   async initShippingAddress() {
     const res = await getAddress({ token: wx.getStorageSync("token") });
 
+    const addressList = res.data.map((item) => {
+      item.addressInfo = `${item.province_name}${item.city_name}${item.area_name}${item.address}`;
+      return item;
+    });
+
     this.setData({
-      addressList: res.data,
+      addressList: addressList,
     });
   },
   onPullDownRefresh() {
