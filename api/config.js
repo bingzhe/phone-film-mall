c58ef854ced0c3e44b1087ab86a8497749f86c2a
bingzhe <874177815@qq.com>
2023-03-11 14:11:55 +0800

    index

--- api/config.js
+++ api/config.js
@@ -1,8 +1,11 @@
+const BASE_URL = "http://shop.shouchuangtx.cn";
+
 const API_BASE_URL = "http://shop.shouchuangtx.cn/Api";
 
 const API_FILE_URL = "http://shop.shouchuangtx.cn/Api";
 
 module.exports = {
+  BASE_URL,
   API_BASE_URL,
   API_FILE_URL,
 };
