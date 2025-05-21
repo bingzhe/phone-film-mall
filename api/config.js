2cf255f8a31509b4df34b2172cb6b174aa0c0cf8
bingzhe <874177815@qq.com>
2025-05-21 08:34:10 +0800

    修改api接口

--- api/config.js
+++ api/config.js
@@ -6,9 +6,9 @@
 // const API_BASE_URL = "https://jrdz.qivan.cn/Api";
 // const API_FILE_URL = "https://jrdz.qivan.cn/Api";
 
-const BASE_URL = "https://gechen.xinhengda.top";
-const API_BASE_URL = "https://gechen.xinhengda.top/Api";
-const API_FILE_URL = "https://gechen.xinhengda.top/Api";
+const BASE_URL = "https://gechen.qdidesign.com";
+const API_BASE_URL = "https://gechen.qdidesign.com/Api";
+const API_FILE_URL = "ttps://gechen.qdidesign.com/Api";
 
 module.exports = {
   BASE_URL,
